0f109002fd6b830c589cb9c2fb72403b2b0e9aff
Manish Shrivastav <carttoshoppe@gmail.com>
2026-07-10 14:45:00 -0500

    Add C++ program to demonstrate if-else statements for driving eligibility

---
+++ section9-hw5.cpp
@@ -0,0 +1,34 @@
+/*
+In this exercise you will use an simple if/else  statement to decide if someone 
+can drive. Let's assume that anyone 16 or older can legally drive.
+
+If the person can legally drive, your program should display, "Yes - you can drive!" 
+using a cout  statement. You do not have to provide a '\n' or endl .
+
+If the person cannot legally drive, then your program should display, "Sorry, come back 
+in n  years", where n is how many years until the person turns 16 year old.
+*/
+
+#include <iostream>
+
+using namespace std;
+
+void can_you_drive(int age)
+{
+    const int driving_age {16};
+
+    if (age >= driving_age)
+    {
+        cout <<" Yes - you can drive!";
+    }
+    else
+    {
+        cout << "Sorry come back in " << driving_age - age << " years";
+    }
+}
+
+int main()
+{
+    can_you_drive(12);
+    return 0;
+}
