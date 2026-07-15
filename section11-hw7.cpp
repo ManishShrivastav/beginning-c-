ec3e2775bf90f7c52b78599db7fe7f3a73404f68
ManishShrivastav <carttoshoppe@gmail.com>
2026-07-15 15:17:16 -0500

    Add C++program to convert temperature

---
+++ section11-hw7.cpp
@@ -0,0 +1,46 @@
+/*
+In this exercise you will create a program that will be used to convert 
+Fahrenheit temperatures to Celsius and Kelvin temperatures through the use of two functions.
+
+For this program, assume that temperature will be represented as a double value.
+
+Begin by defining the function prototypes for the functions fahrenheit_to_celsius 
+and fahrenheit_to_kelvin which are both passed a double value and return a double value.
+
+Now, at the bottom of the file, write the full definitions of both functions.
+
+The function fahrenheit_to_celsius is passed a Fahrenheit temperature and returns 
+a rounded Celsius temperature. You may use the <cmath> function round in order to 
+round the return value. The formula to convert Fahrenheit to Celsius is (5.0/9.0)*(temperature - 32). 
+
+The function fahrenheit_to_kelvin is passed a Fahrenheit temperature and returns a 
+rounded Kelvin temperature. The formula to convert Fahrenheit to Kelvin is (5.0/9.0)*(temperature - 32) + 273.
+*/
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+// Prototypes
+double fahrenheit_to_celsius(double);
+double fahrenheit_to_kelvin(double);
+
+void temperature_conversion(double fahrenheit_temperature)
+{
+
+    double celsius_temperature = fahrenheit_to_celsius(fahrenheit_temperature);
+    double kelvin_temperature = fahrenheit_to_kelvin(fahrenheit_temperature);
+    
+    cout << "The fahrenheit temperature " << fahrenheit_temperature << " degrees is equivalent to " << celsius_temperature << " degrees celsius and " << kelvin_temperature << " degrees kelvin.";
+}
+
+double fahrenheit_to_celsius(double temperature)
+{
+    return round(((temperature - 32)*5)/9);
+} 
+
+double fahrenheit_to_kelvin(double temperature)
+{
+    return round(((temperature - 32)*5)/9 + 273);
+}
