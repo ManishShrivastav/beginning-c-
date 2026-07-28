0bffb1f360bd25b7ebec803e7a248cbbc4e016fc
ManishShrivastav <carttoshoppe@gmail.com>
2026-07-28 17:31:37 -0500

    Add C++ program to understand passing a pointer to a function

---
+++ section12-hw13.cpp
@@ -0,0 +1,58 @@
+/*
+Problem Statement:
+
+Pointers are a fundamental aspect of C++. They allow for flexible and efficient programming by giving direct access to memory. You are tasked with writing a function that uses a pointer to multiply an integer value.
+
+Write a function:
+
+void multiply_with_pointer(int* ptr, int multiplier);
+
+
+Input:
+
+    A pointer to an integer (int* ptr). This integer will be between 1 and 100, inclusive.
+
+    An integer (int multiplier) which is the multiplier. This integer will be between 1 and 100, inclusive.
+
+Output:
+
+The function should multiply the integer at the memory address that ptr points to by multiplier.
+
+
+Function signature:
+
+void multiply_with_pointer(int* ptr, int multiplier);
+*/
+
+
+#include <iostream>
+
+using namespace std;
+
+void multiply_with_pointer(int* ptr, int multiplier);
+
+int main()
+{
+    int multiple {};
+    int multiplier {};
+
+    cout << "What is the multiple you want to multiply? ";
+    cin >> multiple;
+
+    cout << "Enter a multiplier: ";
+    cin >> multiplier;
+
+    multiply_with_pointer(&multiple, multiplier);
+
+    cout << "The result is " << multiple; // The value obtained from function is stored in variable "multiple"
+    cout << endl;
+
+    cout << endl;
+    return 0;
+}
+
+void multiply_with_pointer(int* ptr, int multiplier)
+{
+    (*ptr) *= multiplier;
+}
+
