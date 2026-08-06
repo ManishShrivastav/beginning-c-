f19b590ebb45f1da659c516ecd764ea7909f2e4a
Manish Shrivastav <carttoshoppe@gmail.com>
2026-08-06 15:40:59 -0500

    Add Mystring class header file with constructors, assignment operators, and methods

---
+++ Mystring-move-assignment/Mystring.h
@@ -0,0 +1,24 @@
+#ifndef _MYSTRING_H_
+#define _MYSTRING_H_
+
+class Mystring {
+    private:
+        char *str; // pointer to a char[] that holds a C-style string
+
+    public:
+        Mystring(); // No-args constructor
+        Mystring(const char *s); // Overloaded constructor
+        Mystring(const Mystring &source); // Copy constructor
+        Mystring(Mystring &&source); // Move constructor
+
+        Mystring &operator=(const Mystring &rhs); // Copy assignment
+        Mystring &operator=(Mystring &&rhs); // Move assignment
+
+        ~Mystring(); // Destructor
+
+        void display() const; // Display method
+        int get_length() const; // Getter method for length of string
+        const char *get_str() const; // Getter method for string
+};
+
+#endif // _MYSTRING_H_
