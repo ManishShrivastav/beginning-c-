545fc1cea09e4b35dc02210b3e7b75169cf8124e
Manish Shrivastav <carttoshoppe@gmail.com>
2026-08-11 14:38:49 -0500

    Add Mystring class header file with constructors, destructors, and operator overloads

---
+++ Mystring-operator-functions/Mystring.h
@@ -0,0 +1,26 @@
+#ifndef _MYSTRING_H_
+#define _MYSTRING_H_
+
+class Mystring
+{
+    char *str;                                          // pointer to a char[] that holds a C-style string
+public:     
+    Mystring();                                         // No-args constructor
+    Mystring(const char *s);                            // Overloaded constructor
+    Mystring(const Mystring &source);                   // Copy constructor
+    Mystring(Mystring &&source);                        // Move constructor
+    ~Mystring();                                        // Destructor
+
+    Mystring &operator=(const Mystring &rhs);           // Copy assignment
+    Mystring &operator=(Mystring &&rhs);                // Move assignment
+
+    friend Mystring operator-(const Mystring &obj);                         // make lowercase
+    friend Mystring operator+(const Mystring &lhs, const Mystring &rhs);      // concatenate
+    friend bool operator==(const Mystring &lhs, const Mystring &rhs);         // equality
+
+    void display() const;
+    int get_length() const;                             // getters
+    const char *get_str() const;
+};
+
+#endif // _MYSTRING_H_
