5b43ac54916febaa700e0a2236287321b40418b2
Manish Shrivastav <carttoshoppe@gmail.com>
2026-08-12 14:10:18 -0500

    Add Mystring class header with copy/move constructors, destructor, and insertion/extraction overloads

---
+++ Mystring-insertion-extraction/Mystring.h
@@ -0,0 +1,27 @@
+#ifndef _MYSTRING_H_
+#define _MYSTRING_H_
+
+class Mystring {
+    private:
+        char *str; // pointer to a char[] that holds a C-style string
+
+    public:
+        Mystring(); // No-args constructor
+        Mystring(const char *s); // Overloaded constructor
+        Mystring(const Mystring &source); // Copy constructor
+        Mystring(Mystring &&source); // Move constructor
+
+        Mystring &operator=(const Mystring &rhs); // Copy assignment
+        Mystring &operator=(Mystring &&rhs); // Move assignment
+
+        ~Mystring(); // Destructor
+
+        friend std::ostream &operator<<(std::ostream &os, const Mystring &rhs); // Insertion operator
+        friend std::istream &operator>>(std::istream &in, Mystring &rhs);       // Extraction operator
+
+        void display() const; // Display method
+        int get_length() const; // Getter method for length of string
+        const char *get_str() const; // Getter method for string
+};
+
+#endif // _MYSTRING_H_
