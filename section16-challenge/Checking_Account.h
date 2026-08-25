807e9fddcb6f4c15f60a0c9c578c06dd801cd9df
Manish Shrivastav <carttoshoppe@gmail.com>
2026-08-25 13:10:03 -0500

    feat: add Checking_Account class with deposit and withdraw methods

---
+++ section16-challenge/Checking_Account.h
@@ -0,0 +1,19 @@
+#ifndef CHECKING_ACCOUNT_H_
+#define CHECKING_ACCOUNT_H_
+#include <iostream>
+#include <string>
+#include "Account.h"
+
+class Checking_Account : public Account {
+    friend std::ostream &operator<<(std::ostream &os, const Checking_Account &account);
+private:
+    static constexpr const char *def_name = "Unnamed Checking Account";
+    static constexpr double def_balance = 0.0;
+    static constexpr double per_check_fee = 1.50;
+public:
+    Checking_Account(std::string name = def_name, double balance = def_balance);
+    virtual bool deposit(double amount) override;
+    virtual bool withdraw(double amount) override;
+    virtual ~Checking_Account() = default;
+};
+#endif          // _CHECKING_ACCOUNT_H_
