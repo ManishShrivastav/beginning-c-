f7772d39c9ce1e76d6b2cfdc4bd627cb0d5f1f87
ManishShrivastav <carttoshoppe@gmail.com>
2026-08-26 15:14:50 -0500

    Learn std::weak_ptr and break circular references Understand circular references between std::shared_ptrs Use std::weak_ptr to avoid shared ownership cycles Practice forward declarations with mutually dependent classes Observe proper object destruction when the circular reference is broken

---
+++ weak-pointers.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <memory>
+
+using namespace std;
+
+class B;        // forward declaration
+
+class A {
+    std::shared_ptr<B> b_ptr;
+public:
+    void set_B(const std::shared_ptr<B> &b) {
+        b_ptr = b;
+    }
+    A() {cout << "A constructor" << endl;}
+    ~A() {cout << "A destructor" << endl;}
+};
+
+class B{
+    // std::shared_ptr<A> a_ptr;               // make weak to break the circular reference
+    std::weak_ptr<A> a_ptr;                 // make weak to break the circular reference
+public: 
+    void set_A(const std::shared_ptr<A> &a) {
+        a_ptr = a;
+    }
+    B() {cout << "B constructor" << endl;}
+    ~B() {cout << "B destructor" << endl;}  
+};
+
+
+int main() {
+    std::shared_ptr<A> a = std::make_shared<A>();
+    std::shared_ptr<B> b = std::make_shared<B>();
+
+    a->set_B(b);
+    b->set_A(a);
+
+    return 0;
+}
