47f3a6adbe760832abf927e4bf3e68ad29a2bb81
ManishShrivastav <carttoshoppe@gmail.com>
2026-08-27 13:54:49 -0500

    feat: implement Account class hierarchy with exception handling and utility functions

---
+++ ConstructorException/Checking_Account.cpp
@@ -0,0 +1,35 @@
+#include "Checking_Account.h"
+
+Checking_Account::Checking_Account(std::string name, double balance)
+    : Account{name, balance} {
+}
+
+bool Checking_Account::deposit(double amount) {
+    if (amount < 0) {
+        return false;
+    } else {
+        balance += amount;
+        return true;
+    }
+}
+
+bool Checking_Account::withdraw(double amount) {
+    amount += per_check_fee;
+    if (amount < 0 || amount > balance) {
+        return false;
+    } else {
+        balance -= amount;
+        return true;
+    }
+}
+
+void Checking_Account::print(std::ostream &os) const {
+    os.precision(2);
+    os << std::fixed;
+    os << "[Checking Account: " << name << ": " << balance << "]";
+}
+
+// std::ostream &operator<<(std::ostream &os, const Checking_Account &account) {
+//     os << "[Checking Account: " << account.name << ": " << account.balance << "]";
+//     return os;
+// }
