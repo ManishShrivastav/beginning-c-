867e112df7ea203aed19f32d094b1b7365194db8
ManishShrivastav <carttoshoppe@gmail.com>
2026-08-27 14:48:36 -0500

    feat: add InsufficientFundsException class for handling insufficient funds errors

---
+++ Section18-challenge/InsufficientFundsException.h
@@ -0,0 +1,14 @@
+#ifndef _INSUFFICIENT_FUNDS_EXCEPTION_H_
+#define _INSUFFICIENT_FUNDS_EXCEPTION_H_
+#include <exception>
+
+class InsufficientFundsException : public std::exception {
+public:
+    InsufficientFundsException() = default;
+    ~InsufficientFundsException() = default;
+    virtual const char* what() const noexcept {
+        return "Insufficient funds exception.";
+    }
+};
+
+#endif      // _INSUFFICIENT_FUNDS_EXCEPTION_H_
