cee49be753ff0fe0f314dbfec02490d7065423c9
ManishShrivastav <carttoshoppe@gmail.com>
2026-08-27 12:00:04 -0500

    Learn exception handling with division-by-zero validation
    Add a check to prevent division by zero when calculating miles per gallon.
    Display an error message when gallons is zero.

---
+++ mph.cpp
@@ -0,0 +1,28 @@
+// Section 18
+
+#include <iostream>
+
+int main() {
+    int miles {};
+    int gallons {};
+    double miles_per_gallon {};
+
+    std::cout << "Enter the number of miles: ";
+    std::cin >> miles;
+    std::cout << "Enter the number of gallons: ";
+    std::cin >> gallons;
+
+    // miles_per_gallon = miles / gallons;
+    if (gallons != 0) {
+        miles_per_gallon = static_cast<double>(miles) / gallons;
+        std::cout << "Miles per gallon: " << miles_per_gallon << std::endl;
+    } else {
+        std::cerr << "Sorry, can't divide by zero." << std::endl;
+    }
+    // miles_per_gallon = static_cast<double>(miles) / gallons;
+    // std::cout << "Miles per gallon: " << miles_per_gallon << std::endl;
+
+    std::cout << "Bye!" << std::endl;
+
+    return 0;
+}
