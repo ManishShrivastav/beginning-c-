ac0c32dfd2efd9e052a742a4542a9c31c41f1788
ManishShrivastav <carttoshoppe@gmail.com>
2026-08-27 14:42:27 -0500

    feat: implement Account class hierarchy with exception handling and utility functions

---
+++ Section18-challenge/IllegalBalanceException.h
@@ -0,0 +1,15 @@
+#ifndef _ILLEGAL_BALANCE_EXCEPTION_H_
+#define _ILLEGAL_BALANCE_EXCEPTION_H_
+
+#include <exception>
+
+class IllegalBalanceException : public std::exception {
+public:
+    IllegalBalanceException() = default;
+    ~IllegalBalanceException() = default;
+    virtual const char* what() const noexcept {
+        return "Illegal balance exception.";
+    }
+};
+
+#endif      // _ILLEGAL_BALANCE_EXCEPTION_H_
